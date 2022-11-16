961d57825dc6c5715b9a0d33e142908cae26f69a
orya <orya.sp@gmail.com>
2022-11-16 16:28:32 +0200

    Edit route name

--- server/routes/categories.py
+++ server/routes/categories.py
@@ -2,15 +2,13 @@ from fastapi import status, APIRouter
 from fastapi.responses import JSONResponse
 from models import categories as Categories
 
-router = APIRouter(
-    prefix="/categories"
-)
+router = APIRouter()
 
-@router.get('/')
+@router.get('/categories_breakdown')
 def get_categories_breakdown():
     try:
         breakdown = Categories.get_breakdown()
         return breakdown
     except Exception as e:
         return JSONResponse({"Error": e},
-                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
