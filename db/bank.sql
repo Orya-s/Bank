9334fba521cc4aeefe672550cc1b3c4d7e42004e
orya <orya.sp@gmail.com>
2022-11-16 18:45:26 +0200

    Delete categories table

--- db/bank.sql
+++ db/bank.sql
@@ -1,8 +1,6 @@
 use bank;
 
 drop table IF EXISTS transactions;
-drop table IF EXISTS categories;
-
 
 CREATE TABLE IF NOT EXISTS transactions(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -10,9 +8,3 @@ CREATE TABLE IF NOT EXISTS transactions(
     vendor VARCHAR(20),
     category VARCHAR(20)
 ); 
-
--- CREATE TABLE IF NOT EXISTS categories(
---     category VARCHAR(20) PRIMARY KEY,
---     amount INT,
---     transactions_number INT
--- ); 
