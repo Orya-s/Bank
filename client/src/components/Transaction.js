d3a68781f8059f90f935bae1f6badd02e6fe49f5
orya <orya.sp@gmail.com>
2022-11-20 21:48:53 +0200

    Add Card component to Transaction component

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,18 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { Card, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/Transactions.css'
 
 function Transaction(props) {
 
     const deleteTransaction = () => {
         props.deleteTransaction(props.data.id)
     }
+
+    const getClass = () => {
+        return props.data.amount < 0 ? 'withdraw' : 'deposit'
+    }
     
     return ( 
-        // color in red / green 
-        <div>
-            <p>amount: {props.data.amount}</p>
-            <p>vendor: {props.data.vendor}</p>  
-            <p>category: {props.data.category}</p>
-            <button onClick={deleteTransaction}>Delete</button>
+        <div className='card-container'>
+            <Card className='card'>
+                <Card.Header as="h5" className={getClass()}>{props.data.amount}</Card.Header>
+                <Card.Body>
+                    <Card.Title>{props.data.category}</Card.Title>
+                    <Card.Text>
+                    {props.data.vendor}
+                    </Card.Text>
+                    <Button variant="primary" onClick={deleteTransaction}>Delete</Button>
+                </Card.Body>
+            </Card>
             <br/>
         </div>
     );
