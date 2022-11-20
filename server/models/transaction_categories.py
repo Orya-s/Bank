7a7211d23e913897070ac0a8a7e1441eae853770
orya <orya.sp@gmail.com>
2022-11-20 21:45:08 +0200

    Edit connection in categories_breakdown

--- server/models/transaction_categories.py
+++ server/models/transaction_categories.py
@@ -1,17 +1,16 @@
 import pymysql
 
-connection = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="",
-    db="bank",
-    charset="utf8",
-    cursorclass=pymysql.cursors.DictCursor
-)
 
 
 def get_breakdown():
-    connection.ping()
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        db="bank",
+        charset="utf8",
+        cursorclass=pymysql.cursors.DictCursor
+    )
     query = "SELECT category, SUM(amount) as amount FROM transactions GROUP BY category"
     with connection.cursor() as cursor:
         cursor.execute(query)
